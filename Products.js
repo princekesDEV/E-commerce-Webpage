3712b7e78c8e260b56feb5882512b701f737219d
princekesDEV <kesprince1250@gmail.com>
2024-02-10 11:29:24 +0530

    Made a E-commerce Website using Fetch

---
+++ Products.js
@@ -0,0 +1,41 @@
+let bag = [];
+fetch("https://fakestoreapi.com/products")
+  .then((res) =>
+    res.json().then((e) => {
+      bag = e;
+      displayCards(e);
+      console.log(bag);
+    })
+  )
+  .catch((err) => console.log(err));
+let cart_arr = JSON.parse(localStorage.getItem("Cart_Products")) || [];
+function displayCards(e) {
+  e.forEach((elem) => {
+    let card = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = elem.image;
+    let title = document.createElement("p");
+    title.innerText = elem.title;
+    let price = document.createElement("h3");
+    price.innerText = elem.price;
+    let rating = document.createElement("p");
+    rating.innerText = `Rating:- ${elem.rating.rate}`;
+    let addToCart = document.createElement("button");
+    addToCart.addEventListener("click", () => {
+      cart_arr.push(elem);
+      localStorage.setItem("Cart_Products", JSON.stringify(cart_arr));
+    });
+    addToCart.innerText = "Add To Cart";
+    document.querySelector("#container").append(card);
+    card.append(image, title, price, rating, addToCart);
+  });
+}
+function search() {
+  document.querySelector("#container").innerHTML = "";
+  let search_value = document.querySelector("input").value;
+
+  let new_data = bag.filter((elem) => {
+    return elem.title.toLowerCase().includes(search_value.toLowerCase());
+  });
+  displayCards(new_data);
+}
